9fa101a0610e066d865aa2cde0f1e1e17f7f00fd
ronald majoor <r.majoor@gmail.com>
2023-09-03 07:10:28 +0200

    3-9-23

---
+++ KennisMainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace KenisBank
+{
+    public partial class KennisMainForm : Form
+    {
+        Regel regels = new Regel();
+        public KennisMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KennisMainForm_Shown(object sender, EventArgs e)
+        {
+            // laad main.xml
+            regels.Laad(@"Data\main.xml");
+        }
+
+
+    }
+}
---
+++ Regel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace KenisBank
+{
+    internal enum type
+    {
+        PaginaNaam,
+        Link,
+        Hoofdstuk
+    }
+
+    [Serializable]
+    public class Regel
+    {
+        public List<Regel> ListRegels = new List<Regel>();
+        public Regel() { }
+        public Regel(string tekst, int type)
+        {
+            tekst_ = tekst;
+            type_ = type;
+        }
+        public string tekst_ { set; get; }
+        public int type_ { set; get; }
+
+        public void Laad(string file)
+        {
+            try
+            {
+                string xmlTekst = File.ReadAllText(file);
+                ListRegels.Clear();
+                ListRegels = FromXML<List<Regel>>(xmlTekst);
+            }
+            catch { }
+        }
+
+        private string ToXML<T>(T obj)
+        {
+            using (StringWriter stringWriter = new StringWriter(new StringBuilder()))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(stringWriter, obj);
+                return stringWriter.ToString();
+            }
+        }
+
+        private static T FromXML<T>(string xml)
+        {
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                return (T)serializer.Deserialize(stringReader);
+            }
+        }
+    }
+}
