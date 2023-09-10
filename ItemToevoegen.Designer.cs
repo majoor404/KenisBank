b07eea9fd017977958a34c2ac5e3cb2c237286c8
ronald majoor <r.majoor@gmail.com>
2023-09-10 19:51:31 +0200

    start item invoer scherm

---
+++ ItemToevoegen.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace KenisBank
+{
+    partial class ItemToevoegen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHoofdstuk = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kies welk Soort Item u wilt toevoegen op pagina. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(604, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dit nieuwe item komt onder aan de pagina, kunt u later verslepen naar juiste plek" +
+    " op pagina.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hoofdstuk Titel",
+            "Link naar document of directory",
+            "Stuk tekst"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(601, 26);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Titel van Hoofdstuk";
+            this.label3.Visible = false;
+            // 
+            // textBoxHoofdstuk
+            // 
+            this.textBoxHoofdstuk.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHoofdstuk.Location = new System.Drawing.Point(28, 167);
+            this.textBoxHoofdstuk.Name = "textBoxHoofdstuk";
+            this.textBoxHoofdstuk.Size = new System.Drawing.Size(601, 24);
+            this.textBoxHoofdstuk.TabIndex = 4;
+            this.textBoxHoofdstuk.Visible = false;
+            // 
+            // ItemToevoegen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 746);
+            this.Controls.Add(this.textBoxHoofdstuk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ItemToevoegen";
+            this.Text = "Item Toevoegen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHoofdstuk;
+    }
+}
--- KennisMainForm.Designer.cs
+++ KennisMainForm.Designer.cs
@@ -29,71 +29,67 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.buttonEdit = new System.Windows.Forms.Button();
-            this.buttonVoegToe = new System.Windows.Forms.Button();
-            this.buttonSave = new System.Windows.Forms.Button();
             this.panelMain = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.editPaginaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editModeAanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.voegItemToaAanPaginaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.buttonEdit);
-            this.panel1.Controls.Add(this.buttonVoegToe);
-            this.panel1.Controls.Add(this.buttonSave);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(12, 31);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1884, 70);
+            this.panel1.Size = new System.Drawing.Size(1570, 44);
             this.panel1.TabIndex = 0;
-            this.panel1.Click += new System.EventHandler(this.panel1_Click);
-            // 
-            // buttonEdit
-            // 
-            this.buttonEdit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.buttonEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEdit.Location = new System.Drawing.Point(1727, 12);
-            this.buttonEdit.Name = "buttonEdit";
-            this.buttonEdit.Size = new System.Drawing.Size(144, 44);
-            this.buttonEdit.TabIndex = 2;
-            this.buttonEdit.Text = "Edit Mode";
-            this.buttonEdit.UseVisualStyleBackColor = true;
-            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
-            // 
-            // buttonVoegToe
-            // 
-            this.buttonVoegToe.Enabled = false;
-            this.buttonVoegToe.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonVoegToe.Location = new System.Drawing.Point(182, 12);
-            this.buttonVoegToe.Name = "buttonVoegToe";
-            this.buttonVoegToe.Size = new System.Drawing.Size(144, 44);
-            this.buttonVoegToe.TabIndex = 1;
-            this.buttonVoegToe.Text = "Voeg Toe";
-            this.buttonVoegToe.UseVisualStyleBackColor = true;
-            this.buttonVoegToe.Click += new System.EventHandler(this.buttonVoegToe_Click);
-            // 
-            // buttonSave
-            // 
-            this.buttonSave.Enabled = false;
-            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSave.Location = new System.Drawing.Point(12, 12);
-            this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(144, 44);
-            this.buttonSave.TabIndex = 0;
-            this.buttonSave.Text = "Save";
-            this.buttonSave.UseVisualStyleBackColor = true;
-            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // panelMain
             // 
             this.panelMain.AutoScroll = true;
             this.panelMain.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMain.Location = new System.Drawing.Point(0, 70);
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Location = new System.Drawing.Point(12, 91);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(1884, 891);
+            this.panelMain.Size = new System.Drawing.Size(1602, 701);
             this.panelMain.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editPaginaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1884, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // editPaginaToolStripMenuItem
+            // 
+            this.editPaginaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editModeAanToolStripMenuItem,
+            this.voegItemToaAanPaginaToolStripMenuItem});
+            this.editPaginaToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editPaginaToolStripMenuItem.Name = "editPaginaToolStripMenuItem";
+            this.editPaginaToolStripMenuItem.Size = new System.Drawing.Size(120, 24);
+            this.editPaginaToolStripMenuItem.Text = "Pas pagina aan";
+            // 
+            // editModeAanToolStripMenuItem
+            // 
+            this.editModeAanToolStripMenuItem.CheckOnClick = true;
+            this.editModeAanToolStripMenuItem.Name = "editModeAanToolStripMenuItem";
+            this.editModeAanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editModeAanToolStripMenuItem.Text = "Edit Mode Aan";
+            this.editModeAanToolStripMenuItem.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // voegItemToaAanPaginaToolStripMenuItem
+            // 
+            this.voegItemToaAanPaginaToolStripMenuItem.Enabled = false;
+            this.voegItemToaAanPaginaToolStripMenuItem.Name = "voegItemToaAanPaginaToolStripMenuItem";
+            this.voegItemToaAanPaginaToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.voegItemToaAanPaginaToolStripMenuItem.Text = "Voeg Item toe aan pagina";
+            this.voegItemToaAanPaginaToolStripMenuItem.Click += new System.EventHandler(this.buttonVoegToe_Click);
             // 
             // KennisMainForm
             // 
@@ -102,22 +98,28 @@
             this.ClientSize = new System.Drawing.Size(1884, 961);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "KennisMainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kennis Bank";
             this.Shown += new System.EventHandler(this.KennisMainForm_Shown);
-            this.panel1.ResumeLayout(false);
+            this.Resize += new System.EventHandler(this.KennisMainForm_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button buttonSave;
-        private System.Windows.Forms.Button buttonEdit;
-        private System.Windows.Forms.Button buttonVoegToe;
         private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editPaginaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editModeAanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem voegItemToaAanPaginaToolStripMenuItem;
     }
 }
 
